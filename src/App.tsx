22c9cb48a2a47f4f1e8b56295caad7faaf7e2de5
hwatton <66825106+hwatton@users.noreply.github.com>
2022-02-23 16:23:53 +0000

    lab21 complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import ProjectsPage from './projects/ProjectsPage';
+import {
+  BrowserRouter as Router, Route, NavLink, Routes
+} from 'react-router-dom'
+/** above! Switch is now deprecated! swapped in <Routes/>. 
+ * docs suggest minimal fixes required, 
+ * just element itsef can be passed and path can be '/' 
+ * without need of "exact" + a few bits about nesting them in the jsx*/
 
+import HomePage from './home/HomePage'
 import './App.css';
 
 function App() {
   return (
+    <Router>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects/" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
     <div className="container">
-      <ProjectsPage/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/projects" element={<ProjectsPage/>}/>
+      </Routes>
     </div>
+    </Router>
   )
 }
 
---
+++ src/home/HomePage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function HomePage() {
+    return <h2>Hoime</h2>
+}
+
+export default HomePage
